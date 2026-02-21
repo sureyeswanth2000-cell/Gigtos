f5c5b4d4195e23a2aa8e6f8cb64bcde02e8e7d81
sureyeswanth2000-cell <sure.yeswanth2000@gmail.com>
2026-02-21 10:56:38 +0530

    Initialize Firebase app and export auth and db
    Firebasejs

---
+++ firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMJvNKvgwfEvymuLaXhGQwJr-Id4yExYU",
+  authDomain: "gigto-c0c83.firebaseapp.com",
+  projectId: "gigto-c0c83",
+  storageBucket: "gigto-c0c83.firebasestorage.app",
+  messagingSenderId: "190454381677",
+  appId: "1:190454381677:web:458b1638c984ababcdd364",
+  measurementId: "G-3WQKM1M1F9"
+};
+
+
+
+
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);

daff54f37b13b08a6834fb0e6aceb8473f42219f
sureyeswanth2000-cell <sure.yeswanth2000@gmail.com>
2026-02-24 21:07:41 +0530

    New

--- components/header.js
+++ components/header.js
@@ -1,24 +1,54 @@
-onAuthStateChanged(auth, async (user) => {
+import { auth, db } from "../firebase.js";
+import { onAuthStateChanged, signOut } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { doc, getDoc } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+export function renderHeader() {
+
+  const placeholder = document.getElementById("header-placeholder");
+
+  placeholder.innerHTML = `
+    <header class="global-header">
+      <div class="hamburger" onclick="openMenu()">☰</div>
+      <div class="logo" onclick="goHome()">Gigto</div>
+      <div id="headerProfile" class="header-profile hidden"></div>
+    </header>
+  `;
+
+  initAuthHeader();
+}
+
+window.goHome = function() {
+  window.location.href = "index.html";
+};
+
+async function initAuthHeader() {
 
   const profile = document.getElementById("headerProfile");
 
-  if (!user) {
-    profile.innerText = "Login";
+  onAuthStateChanged(auth, async (user) => {
+
+    if (!user) {
+      profile.innerText = "Login";
+      profile.classList.remove("hidden");
+      profile.onclick = () => {
+        window.location.href = "auth.html";
+      };
+      return;
+    }
+
+    const userRef = doc(db, "users", user.uid);
+    const snap = await getDoc(userRef);
+
+    let name = user.phoneNumber;
+
+    if (snap.exists() && snap.data().name) {
+      name = snap.data().name;
+    }
+
+    profile.innerText = "👤 " + name;
     profile.classList.remove("hidden");
-    profile.onclick = () => {
-      window.location.href = "auth.html";
-    };
-    return;
-  }
-
-  const userRef = doc(db, "users", user.uid);
-  const snap = await getDoc(userRef);
-
-  let name = user.phoneNumber;
-  if (snap.exists() && snap.data().name) {
-    name = snap.data().name;
-  }
-
-  profile.innerText = "👤 " + name;
-  profile.onclick = () => window.location.href = "profile.html";
-});
+    profile.onclick = () => window.location.href = "profile.html";
+  });
+}
